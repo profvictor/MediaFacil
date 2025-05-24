8ff706da6e7987b42f7f4896f6e9d1ad762370c5
Victor <78748752+profvictor@users.noreply.github.com>
2025-05-24 18:39:53 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,47 +1,61 @@
-// This is the "Offline page" service worker
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-const CACHE = "pwabuilder-page";
-
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "ToDo-replace-this-name.html";
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
-
-self.addEventListener('install', async (event) => {
+// On install - caching the application shell
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
+    caches.open('sw-cache').then(function(cache) {
+      // cache any static files that make up the application shell
+      return cache.add('index.html');
+    })
   );
 });
 
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
+// On network request
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    // Try the cache
+    caches.match(event.request).then(function(response) {
+      //If response found return it, else fetch again
+      return response || fetch(event.request);
+    })
+  );
+});
 
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
-
-        if (preloadResp) {
-          return preloadResp;
-        }
-
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
-
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
+// Use background sync for a rich offline experience
+self.addEventListener('sync', function(event) {
+  if (event.tag === 'sync-data') {
+    event.waitUntil(
+      // Perform synchronization logic here, e.g. update cached data
+      caches.open('sw-cache').then(function(cache) {
+        return cache.addAll([
+          'data.json',
+          'images/image1.jpg',
+          'images/image2.jpg'
+        ]);
+      })
+    );
   }
+});
+
+// On periodic sync
+self.addEventListener('periodicsync', function(event) {
+  if (event.tag === 'periodic-sync') {
+    event.waitUntil(
+      // Perform synchronization logic here, e.g. update cached data
+      caches.open('sw-cache').then(function(cache) {
+        return cache.addAll([
+          'data.json',
+          'images/image1.jpg',
+          'images/image2.jpg'
+        ]);
+      })
+    );
+  }
+});
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    self.registration.showNotification('Install successful', {
+      body: 'The service worker has been installed.',
+      icon: 'path/to/icon.png'
+    })
+  );
 });
