3d6d8c75b99f2f4d204f1344ec6547c22cbd1ac3
Victor <78748752+profvictor@users.noreply.github.com>
2025-05-24 18:24:39 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,67 +1,47 @@
-const CACHE_NAME = 'calc-notas-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/home.html',
-  '/Recuperacao.html',
-  '/SegundaSerie.html',
-  '/Portugues.html',
-  '/PrimeiraSerie.html',
-  '/FaltaQuanto.html',
-  '/TerceiraSerie.html',
-  '/styles.css',
-  '/script.js',
-  '/livro.png',
-  '/iconefalta.png',
-  '/bad-grades.png',
-  '/btnterceira.png',
-  '/btnsegunda.png',
-  '/btnprimeira.png',
-  '/meninoestudando.png'
-];
+// This is the "Offline page" service worker
 
-self.addEventListener('install', event => {
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+const CACHE = "pwabuilder-page";
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "ToDo-replace-this-name.html";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('install', async (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
         }
-        return fetch(event.request)
-          .then(response => {
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-            const responseToCache = response.clone();
-            caches.open(CACHE_NAME)
-              .then(cache => {
-                cache.put(event.request, responseToCache);
-              });
-            return response;
-          });
-      })
-  );
-});
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-}); 
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
+});
